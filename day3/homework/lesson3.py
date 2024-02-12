26f7a37560793544aaf2bcb0d9df20d827106a8f
GiorgiGelashvili7 <giorgi.gelashvili209@gmail.com>
2024-02-12 06:06:44 +0400

    commit

---
+++ day3/homework/lesson3.py
@@ -0,0 +1,43 @@
+# 1 we want to print mother's name and surname
+
+#name = "Natia"
+
+#surname = "Kenchoshvili"
+
+
+#print(name + " " + surname)
+
+
+# 2 we want to print father's name and surname 
+
+#name = "Andro"
+
+#surname = "Gelashvili"
+
+
+#print(name + " " + surname)
+
+# 3 print my sister's name and surname 
+
+#name = "Gvantsa"
+
+#surname = "Gelashvili"
+
+#print(name + " " +  surname)
+
+# 4 print my second sister's name and surname
+
+#name = "Nino"
+
+#surname = "Gelashvili"
+
+#print(name + " " + surname)
+
+
+#print erors 
+
+name = Natia"
+
+surname = (kenchoshvili
+
+print(name - surname)
