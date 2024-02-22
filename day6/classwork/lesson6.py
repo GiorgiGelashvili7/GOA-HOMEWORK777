03b195c946b5167f37f1975a093afdbe9e19804d
GiorgiGelashvili7 <giorgi.gelashvili209@gmail.com>
2024-02-22 23:34:31 +0400

    sixth commit

---
+++ day6/classwork/lesson6.py
@@ -0,0 +1,5 @@
+name = input("what's your name? : ")
+surname = input("what's your surame? : ")
+age = input("what's your age? : ")
+
+print("your name is " + name + " you are " + age + " years old" + " your surname is " + surname)
---
+++ day6/homework/lesson6.py
@@ -0,0 +1,33 @@
+# name = input("what's your name? : ")
+# surname = input("what's your surame? : ")
+# age = input("what's your age? : ")
+
+#print("your name is " + name + " you are " + age + " years old" + " your surname is " + surname)
+
+
+first_name = input("What year were you born? : ")
+second_name = input("What month were you born? : ")
+third_name = input("which number were you born? : ")
+
+
+# we want to write first_name's all mathematical calculations 
+
+print(first_name * 2)
+print(first_name / 3)
+print(first_name + 20)
+print(first_name - 10)
+
+# we want to write second_name's all mathematical calculations
+
+print(second_name * 10)
+print(second_name / 9)
+print(second_name + 80)
+print(second_name - 5)
+
+# we want to write third_name's all mathematical calculations
+
+print(third_name * 20)
+print(third_name / 7)
+print(third_name + 120)
+print(third_name - 50)
+
