8b26a781e2cd787cff1ebddbe4f9688dc37496ec
GiorgiGelashvili7 <giorgi.gelashvili209@gmail.com>
2024-03-07 01:56:59 +0400

    tenth commit

---
+++ day10/homework/lesson10.py
@@ -0,0 +1,52 @@
+# დავპრინტოთ 0-დან 20-მდე ყველა მთელი რიცხვი
+
+# for i in range(0,20):
+#     print(i)
+
+# დავაჯამოთ 50-იდან 100-ის ჩათვლით არსებული ყველა რიცხვი 
+
+# sum = 0
+
+# for num in range(50,101):
+#     sum = sum + num
+#     print(sum)
+
+
+# დავატრიალოთ-10-იდან 10-ის ჩათვლით for ციკლი და იტერაცია მოვახდინოთ 3-ით 
+
+# sum = -10
+
+# for num in range(-10, 0):
+#     sum = sum + 3
+#     print(sum)
+
+
+#მომხმარებელს input-ის გამოყენებით შემოვატანინოთ ორი რიცხვითი მნიშვნელობა. შემდეგ if წინადადებით შევამოწმოთ რომელია უდიდესი და გამოვიყენოთ for ციკლი 
+
+# age = int(input("how old are you : "))
+# date = int(input("which month were you born : "))
+
+# if age > 15:
+#     print("this is higher than the date")
+# else: 
+#     print("Please write correctly")    
+
+# if date < 15:
+#     print("this is lower than the age")
+# else:
+#     print("Please write correctly")
+
+# i = 7
+
+# for num in range (0, 4):
+#     i = i + 2
+#     print(i)
+
+end = int(input("how old are you : "))
+start = int(input("which month were you born : "))
+
+sum = 0
+
+for i in range (9, 16):
+    sum = sum + i
+    print(sum)
