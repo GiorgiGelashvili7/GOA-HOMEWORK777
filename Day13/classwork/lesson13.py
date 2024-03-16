1c6f4367fedd5a192369e98cd8aad5b2aed8d3aa
GiorgiGelashvili7 <giorgi.gelashvili209@gmail.com>
2024-03-16 04:46:40 +0400

    commit

---
+++ Day13/classwork/lesson13.py
@@ -0,0 +1,20 @@
+# age = int(input("please enter your age : "))
+
+# if age >= 0 and age <= 18:
+#     print("you are child")
+# elif age >= 18 and age <= 50:
+#     print("you are adult")
+# else: 
+#     print("you are old")
+
+
+# number = int(input("please enter your favourite number : "))
+    
+# if number > 0:
+#     print("positive")
+# elif number < 0:
+#     print("negative") 
+# else:
+#     print("the number is 0")  
+
+
