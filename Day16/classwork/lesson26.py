9432dfc44f78b151363402708241cf6f3c2b786a
GiorgiGelashvili7 <giorgi.gelashvili209@gmail.com>
2024-03-29 00:56:46 +0400

    commit

---
+++ Day16/classwork/lesson26.py
@@ -0,0 +1,24 @@
+# people = ["gio", "nuca", "andria", "luka", "saba"]
+
+# print(people)
+
+# cars = ["bmw", "porsche", "lamborgini", "toyota", "audi"]
+
+# print(cars)
+
+# name = "giorgi"
+# surname = "gelashvili"
+
+# print(name[0],".",surname[0])
+
+
+# cars = ["bmw", "porsche", "lamborgini", "toyota", "audi"]
+
+# print(len(cars))
+
+# people = ["gio", "nuca", "andria", "luka", "saba"]
+# print(len(people))
+
+
+name = "giorgi"
+print(len(name)-1)
---
+++ Day16/homework/lesson16.py
@@ -0,0 +1,66 @@
+# შევქმნათ სია სადაც შევინახავთ ჩქვენთვის საყვარელ ფილბეს და დავბეჭდოთ მთლიანი სია/ კონკრეტული ფილმი
+
+# films = ["Bad Boys For Life", "Karmouz War", "Mechanic: Resurrection", "Triple Threat", "Boat Trip"]
+
+# print(films)
+
+
+#შევქნათ 2 სია და გავაერთიანოთ ისინი
+
+# films = ["Bad Boys For Life", "Karmouz War", "Mechanic: Resurrection", "Triple Threat", "Boat Trip"]
+
+# cars = ["bmw", "mercedes-benz", "porsche", "lamborgini", "toyota","hyndai"]
+
+# print(films + cars)
+
+
+#შევქმნათ სტრინგი მაგ. თქვენი სახელი, და გამოიტანეთ სახელის პირველი და ბოლო სიმბოლო
+
+# name = "giorgi"
+
+# print(name[0] + name[5])
+
+
+#შექმენით სია და სტრინგი ხოლო შემდეგ დაბეჭდეთ len ფუნქციის გამოყენებით მათი ზომა
+
+
+# films = ["Bad Boys For Life", "Karmouz War", "Mechanic: Resurrection", "Triple Threat", "Boat Trip"]
+
+
+# films1 = "Bad Boys For Life"
+# films2 = "Karmouz War"
+# films3 = "Mechanic: Resurrection"
+# films4 = "Triple Threat"
+# films5 = "Boat Trip"
+
+
+# print(len(films1))
+# print(len(films2))
+# print(len(films3))
+# print(len(films4))      
+# print(len(films5))
+
+
+#შეცვალეთ სიიდან რომელიმე მნიშვნელობა 
+
+# films = ["Spirited", "Fall", "Mechanic: Resurrection", "Triple Threat", "Grown Ups"]
+
+# films1 = "Spirited"
+# films2 = "Fall"
+# films3 = "Mechanic: Resurrection"
+# films4 = "Triple Threat"
+# films5 = "Grown ups"
+
+# print(len(films1))
+# print(len(films2))
+# print(len(films3))
+# print(len(films4))      
+# print(len(films5))
+
+
+#შექმენით რიცხვების სია სადაც 10 რიცხვს შეიტანთ, გამოიტანეთ პირელ ინდექსა და ბოლო ინდექსზე მყოფი ელემენტების ჯამი
+
+
+numbers = [23, 54, 77, 11, 2, 99, 4, 30, 44, 777]
+
+print(numbers[0] + numbers[9])
